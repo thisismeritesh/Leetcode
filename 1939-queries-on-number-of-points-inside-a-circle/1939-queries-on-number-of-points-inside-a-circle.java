0fa1cf04c9e1f4d8597d2a3fe1988a1093d37372
thisismeritesh <122709026+thisismeritesh@users.noreply.github.com>
2025-02-18 15:15:34 +0530

    Time: 15 ms (97.65%), Space: 45.3 MB (75.84%) - LeetHub

--- 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.java
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.java
@@ -2,22 +2,17 @@ class Solution {
     // (x - h)² + (y - k)² = r² where (h, k) represents the center of the circle and r is the radius. 
     public int[] countPoints(int[][] points, int[][] queries) {
         int[] res=new int[queries.length];
-        int index=0;
-        for(int[] q:queries){
-            int count=0;
-            for(int[] p:points){
-                if(check(p,q))res[index]++;
+    
+        for(int i=0;i<queries.length;i++){
+            int h=queries[i][0], k=queries[i][1], r=queries[i][2];
+            for(int j=0;j<points.length;j++){
+                int x=points[j][0],y=points[j][1];
+                if( (x-h)*(x-h) + (y-k)*(y-k)<=r*r )
+                res[i]++;
             }
-            index++;
+          
 
         }
         return res;
-    }
-    public boolean check(int[] points,int[] circle){
-        int x=points[0],y=points[1];
-        int h=circle[0],k=circle[1],r=circle[2];
-        if( ((x-h)*(x-h)) + ((y-k)*(y-k))<=r*r  )return true;
-        return false;
-
     }
 }
